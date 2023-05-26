45e0dbb80f79022d484416d3d4dd4b72f6aa4b6b
Francis Li <mail@francisli.com>
2023-05-26 14:09:54 -0700

    Add page titles

--- client/src/Tours/Tour.js
+++ client/src/Tours/Tour.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { Helmet } from 'react-helmet-async';
 import { Link, useParams } from 'react-router-dom';
 
 import Api from '../Api';
@@ -6,8 +7,10 @@ import FormGroup from '../Components/FormGroup';
 import ResourcesModal from '../Resources/ResourcesModal';
 import VariantTabs from '../Components/VariantTabs';
 import StopsModal from '../Stops/StopsModal';
+import { useStaticContext } from '../StaticContext';
 
 function Tour() {
+  const staticContext = useStaticContext();
   const { TourId } = useParams();
   const [tour, setTour] = useState();
   const [variant, setVariant] = useState();
@@ -76,106 +79,113 @@ function Tour() {
   }
 
   return (
-    <main className="container">
-      {!tour && <div className="spinner-border"></div>}
-      {tour && (
-        <>
-          <div className="row">
-            <div className="col-md-6">
-              <h1 className="mb-3">{tour.names[tour.variants[0].code]}</h1>
-              <form>
-                <FormGroup plaintext name="link" label="Link" record={tour} />
-                <VariantTabs variants={tour.variants} current={variant} setVariant={setVariant} />
-                <FormGroup plaintext name="name" label="Name" value={tour.names[variant.code]} />
-                <FormGroup plaintext name="description" label="Description" value={tour.descriptions[variant.code]} />
+    <>
+      <Helmet>
+        <title>
+          {tour?.names[tour.variants[0].code] ?? ''} - {staticContext.env.REACT_APP_SITE_TITLE}
+        </title>
+      </Helmet>
+      <main className="container">
+        {!tour && <div className="spinner-border"></div>}
+        {tour && (
+          <>
+            <div className="row">
+              <div className="col-md-6">
+                <h1 className="mb-3">{tour.names[tour.variants[0].code]}</h1>
+                <form>
+                  <FormGroup plaintext name="link" label="Link" record={tour} />
+                  <VariantTabs variants={tour.variants} current={variant} setVariant={setVariant} />
+                  <FormGroup plaintext name="name" label="Name" value={tour.names[variant.code]} />
+                  <FormGroup plaintext name="description" label="Description" value={tour.descriptions[variant.code]} />
+                  <div className="mb-3">
+                    <Link className="btn btn-primary" to="edit">
+                      Edit
+                    </Link>
+                  </div>
+                </form>
+                <h2>Assets</h2>
+                <table className="table table-striped">
+                  <thead>
+                    <tr>
+                      <th>#</th>
+                      <th>Type</th>
+                      <th>Name</th>
+                      <th>Timeline</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {!resources && (
+                      <tr>
+                        <td colSpan="4">
+                          <div className="spinner-border"></div>
+                        </td>
+                      </tr>
+                    )}
+                    {resources?.length === 0 && (
+                      <tr>
+                        <td colSpan="4">No assets yet.</td>
+                      </tr>
+                    )}
+                    {resources?.map((r, i) => (
+                      <tr key={r.id}>
+                        <td>{i + 1}</td>
+                        <td>{r?.Resource.type}</td>
+                        <td>{r?.Resource.name}</td>
+                        <td></td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
                 <div className="mb-3">
-                  <Link className="btn btn-primary" to="edit">
-                    Edit
-                  </Link>
+                  <button onClick={() => setShowingResourcesModal(true)} type="button" className="btn btn-primary">
+                    Add Asset
+                  </button>
                 </div>
-              </form>
-              <h2>Assets</h2>
-              <table className="table table-striped">
-                <thead>
-                  <tr>
-                    <th>#</th>
-                    <th>Type</th>
-                    <th>Name</th>
-                    <th>Timeline</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {!resources && (
+                <h2>Stops</h2>
+                <table className="table table-striped">
+                  <thead>
                     <tr>
-                      <td colSpan="4">
-                        <div className="spinner-border"></div>
-                      </td>
+                      <th>#</th>
+                      <th>Name</th>
+                      <th>Address</th>
                     </tr>
-                  )}
-                  {resources?.length === 0 && (
-                    <tr>
-                      <td colSpan="4">No assets yet.</td>
-                    </tr>
-                  )}
-                  {resources?.map((r, i) => (
-                    <tr key={r.id}>
-                      <td>{i + 1}</td>
-                      <td>{r?.Resource.type}</td>
-                      <td>{r?.Resource.name}</td>
-                      <td></td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-              <div className="mb-3">
-                <button onClick={() => setShowingResourcesModal(true)} type="button" className="btn btn-primary">
-                  Add Asset
-                </button>
-              </div>
-              <h2>Stops</h2>
-              <table className="table table-striped">
-                <thead>
-                  <tr>
-                    <th>#</th>
-                    <th>Name</th>
-                    <th>Address</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {!stops && (
-                    <tr>
-                      <td colSpan="4">
-                        <div className="spinner-border"></div>
-                      </td>
-                    </tr>
-                  )}
-                  {stops?.length === 0 && (
-                    <tr>
-                      <td colSpan="4">No stops yet.</td>
-                    </tr>
-                  )}
-                  {stops?.map((ts, i) => (
-                    <tr key={ts.id}>
-                      <td>{i + 1}</td>
-                      <td>{ts.Stop.names[ts.Stop.variants[0].code]}</td>
-                      <td>{ts.Stop.address}</td>
-                      <td></td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-              <div className="mb-3">
-                <button onClick={() => setShowingStopsModal(true)} type="button" className="btn btn-primary">
-                  Add Stop
-                </button>
+                  </thead>
+                  <tbody>
+                    {!stops && (
+                      <tr>
+                        <td colSpan="4">
+                          <div className="spinner-border"></div>
+                        </td>
+                      </tr>
+                    )}
+                    {stops?.length === 0 && (
+                      <tr>
+                        <td colSpan="4">No stops yet.</td>
+                      </tr>
+                    )}
+                    {stops?.map((ts, i) => (
+                      <tr key={ts.id}>
+                        <td>{i + 1}</td>
+                        <td>{ts.Stop.names[ts.Stop.variants[0].code]}</td>
+                        <td>{ts.Stop.address}</td>
+                        <td></td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+                <div className="mb-3">
+                  <button onClick={() => setShowingStopsModal(true)} type="button" className="btn btn-primary">
+                    Add Stop
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
-        </>
-      )}
-      <ResourcesModal isShowing={isShowingResourcesModal} onHide={onHideResourcesModal} onSelect={onSelectResource} />
-      <StopsModal isShowing={isShowingStopsModal} onHide={onHideStopsModal} onSelect={onSelectStop} />
-    </main>
+          </>
+        )}
+        <ResourcesModal isShowing={isShowingResourcesModal} onHide={onHideResourcesModal} onSelect={onSelectResource} />
+        <StopsModal isShowing={isShowingStopsModal} onHide={onHideStopsModal} onSelect={onSelectStop} />
+      </main>
+    </>
   );
 }
 export default Tour;
