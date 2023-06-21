747409c2505f15e365b666b1295e2c5f7e08352f
Francis Li <mail@francisli.com>
2023-06-21 10:56:28 -0700

    Subdomain parsing, find team and tour

--- server/routes/published/index.js
+++ server/routes/published/index.js
@@ -1,9 +1,32 @@
 const express = require('express');
+const { StatusCodes } = require('http-status-codes');
+
+const models = require('../../models');
 
 const router = express.Router();
 
-router.get('/*', (req, res) => {
-  res.send('Hello subdomain');
+router.get('/*', async (req, res) => {
+  let [team, tour] = req.subdomains;
+  const isStaging = tour?.toLowerCase() === 'staging';
+  if (isStaging) {
+    [, , tour] = req.subdomains;
+  }
+  if (team && tour) {
+    team = await models.Team.findOne({ where: { link: team } });
+    if (team) {
+      tour = await models.Tour.findOne({
+        where: {
+          TeamId: team.id,
+          link: tour,
+        },
+      });
+      if (tour) {
+        res.send(`found ${team.name} ${tour.names[tour.variants[0].code]}`);
+        return;
+      }
+    }
+  }
+  res.status(StatusCodes.NOT_FOUND).end();
 });
 
 module.exports = router;
