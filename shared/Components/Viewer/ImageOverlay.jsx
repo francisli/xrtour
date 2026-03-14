4f359cd5eacb42e0e8c5b970dfc47a6323939e9a
Francis Li <francisli@users.noreply.github.com>
2026-03-13 18:16:38 -0700

    [Closes #166] Editor user can add a fallback image for photo overlay assets (#183)
    * Add field for collecting image overlay fallback
    
    * First pass, image overlay fallback

--- shared/Components/Viewer/ImageOverlay.jsx
+++ shared/Components/Viewer/ImageOverlay.jsx
@@ -2,7 +2,7 @@ import { useRef, useState } from 'react';
 import { Camera } from 'react-camera-pro';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { faCamera, faImage, faXmark } from '@fortawesome/free-solid-svg-icons';
 import Scrubber from './Scrubber';
 
 import './ImageOverlay.scss';
@@ -10,12 +10,14 @@ import './ImageOverlay.scss';
 function ImageOverlay({ onClose, resource, variant }) {
   const camera = useRef(null);
   const [opacity, setOpacity] = useState(0.5);
+  const [isShowingFallback, setShowingFallback] = useState(false);
 
   const imageURL = resource.Files.find((f) => f.variant === variant.code)?.URL;
+  const fallbackImageURL = resource.Files.find((f) => f.variant === `${variant.code}-fallback`)?.URL;
 
   return (
     <div className="image-overlay">
-      <div className="image-overlay__container">
+      <div className="image-overlay__container" style={{ backgroundImage: isShowingFallback ? `url(${fallbackImageURL})` : 'none' }}>
         <Camera ref={camera} facingMode="environment" />
       </div>
       <div className="image-overlay__image" style={{ backgroundImage: `url(${imageURL})`, opacity }}></div>
@@ -28,6 +30,13 @@ function ImageOverlay({ onClose, resource, variant }) {
         scrub={true}
         onSeek={(value) => setOpacity(1 - value)}
       />
+      {fallbackImageURL && (
+        <div className="image-overlay__fallback">
+          <button onClick={() => setShowingFallback(!isShowingFallback)} className="btn btn-lg btn-primary btn-round">
+            <FontAwesomeIcon icon={isShowingFallback ? faCamera : faImage} />
+          </button>
+        </div>
+      )}
       <div className="image-overlay__close">
         <button onClick={() => onClose()} className="btn btn-lg btn-primary btn-round">
           <FontAwesomeIcon icon={faXmark} />
